95e8a4f1104f023210e223471acd91739e2494a7
ctbier <ctbier@gmail.com>
2025-11-13 14:37:41 -0500

    created python2

---
+++ python_1/paython2.py
@@ -0,0 +1,31 @@
+def get_password_variants(password):
+	pass_variants = []
+	substitutions = {
+		'a': ['@', '4', 'A'],
+		'e': ['3', 'E'],
+		'i': ['1', '!', 'I'],
+		'o': ['0', 'O'],
+		's': ['$', '5', 'S'],
+		't': ['7', 'T'],
+		'z': ['2', 'Z'],
+	}
+
+for i in range (len(password)):
+	if password[1] in substitions:
+		for sub in substitions[password[i]]:
+			pass_variant = password[:i] + sub + passwpord[i+1:]
+			pass_variantes.append(pass_variant)
+
+pass_variants.append(password + '!')
+pass_variants.append(password + '123')
+pass_variants.append(password + '@')
+pass_variants.append(password + '#')
+pass_variants.append(password + '$')
+pass_variants.append(password + '%')
+pass_variants.append(password + '&')
+pass_varients.append(password + '*')
+pass_varients.append(password + '-')
+pass_varrients
+
+
+
